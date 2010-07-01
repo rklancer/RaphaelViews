da91602257142708886756e0dd7fdda98b2c157c
Richard Klancer <rpk@pobox.com>
2010-07-01 16:43:29 -0400

    Trivial 'RectView' class

---
+++ apps/raphael_demo/tests/views/rect.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   RaphaelDemo.RectView Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals RaphaelDemo module test ok equals same stop start */
+
+module("RaphaelDemo.RectView");
+
+// TODO: Replace with real unit test for RaphaelDemo.RectView
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
---
+++ apps/raphael_demo/views/rect.js
@@ -0,0 +1,37 @@
+// ==========================================================================
+// Project:   RaphaelDemo.RectView
+// Copyright: ©2010 Richard Klancer
+// ==========================================================================
+/*globals RaphaelDemo RaphaelViews */
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends RaphaelViews.RaphaelView
+*/
+RaphaelDemo.RectView = RaphaelViews.RaphaelView.extend(
+/** @scope RaphaelDemo.RectView.prototype */ {
+
+  displayProperties: 'x y width height fill stroke'.w(),
+  
+  renderCallback: function (raphael, x, y, width, height, fill, stroke) {
+    return raphael.rect(x, y, width, height).attr({ fill: fill, stroke: stroke });
+  },
+  
+  render: function (context, firstTime) {
+    // note context should always be a RaphaelContext
+    
+    if (firstTime) {
+      context.callback(this, this.renderCallback, this.get('x'), this.get('y'), this.get('width'), 
+        this.get('height'), this.get('fill'), this.get('stroke'));
+      this.renderChildViews(context, firstTime);      // don't forget to render child views
+    }
+    else {
+      var rect = this.get('raphael');
+      rect.attr({ x: this.get('x'), y: this.get('y'), width: this.get('width'), height: this.get('height'), 
+        fill: this.get('fill'), stroke: this.get('stroke') });
+    }
+  }
+
+});
