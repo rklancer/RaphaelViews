6f15447a4751ecce727b3baab783d042dbce7581
Richard Klancer <rpk@pobox.com>
2010-07-01 16:43:48 -0400

    Updated demo app to use the RectView class

--- apps/raphael_demo/resources/main_page.js
+++ apps/raphael_demo/resources/main_page.js
@@ -2,7 +2,7 @@
 // Project:   RaphaelDemo - mainPage
 // Copyright: ©2010 Richard Klancer
 // ==========================================================================
-/*globals RaphaelDemo */
+/*globals RaphaelDemo RaphaelViews */
 
 // This page describes the main user interface for your application.  
 RaphaelDemo.mainPage = SC.Page.design({
@@ -11,13 +11,25 @@ RaphaelDemo.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
+    childViews: 'drawingView'.w(),
     
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
-      value: "Welcome to SproutCore!"
+    drawingView: RaphaelViews.RaphaelCanvasView.design({
+      layout: { centerX: 0, centerY: 0, width: 200, height: 200 },
+      
+      childViews: 'rectView'.w(),
+      
+      rectView: RaphaelDemo.RectView.design({
+        x: 50,
+        y: 50,
+        width: 100,
+        height: 100,
+        fill: '#aa0000',
+        stroke: '#aa0000',
+
+        mouseDown: function () {
+          alert("Thank you for clicking.");
+        }
+      })
     })
   })
 
