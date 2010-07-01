77acccb90280ff3d1d3c127cb013b738715b60fd
Richard Klancer <rpk@pobox.com>
2010-07-01 14:34:17 -0400

    Core namespace object

---
+++ frameworks/RaphaelViews/core.js
@@ -0,0 +1,7 @@
+// ==========================================================================
+// Project:   RaphaelViews
+// Copyright: ©2010 Richard Klancer and Concord Consortium
+// ==========================================================================
+/*globals */
+
+var RaphaelViews = SC.Object.create();
