ec622396756ff06e5aad17432d6ce309bd133dca
Richard Klancer <rpk@pobox.com>
2010-07-01 22:54:49 -0400

    Fancy up DataPointView a little

--- apps/raphael_demo/views/data_point.js
+++ apps/raphael_demo/views/data_point.js
@@ -20,21 +20,29 @@ RaphaelDemo.DataPointView = RaphaelViews.RaphaelView.extend(
   radius: 3,
   isSelected: NO,
   isEnabled: YES,
+  selectedFill: '#0000aa',
+  selectedStroke: '#0000aa',
   
   renderCallback: function (raphaelCanvas, x, y, radius, fill, stroke) {
     return raphaelCanvas.circle(x, y, radius).attr({ fill: fill, stroke: stroke });
   },
   
   render: function (context, firstTime) {
-
+    console.log('DataPointView render(), firstTime = ', firstTime);
+    
+    var isSelected = this.get('isSelected');
+    
+    var fill = isSelected ?  this.get('selectedFill') : this.get('fill');
+    var stroke = isSelected ? this.get('selectedStroke') : this.get('stroke');
+    
     if (firstTime) {
       context.callback(this, this.renderCallback, this.getPath('content.x') || 0, this.getPath('content.y') || 0, 
-        this.get('radius'), this.get('fill'), this.get('stroke'));
+        this.get('radius'), fill, stroke);
     }
     else {
       var circle = this.get('raphaelObject');
       circle.attr({ cx: this.getPath('content.x') || 0, cy: this.getPath('content.y') || 0, radius: this.get('radius'), 
-        fill: this.get('fill'), stroke: this.get('stroke') });
+        fill: fill, stroke: stroke });
     }
   }
 
