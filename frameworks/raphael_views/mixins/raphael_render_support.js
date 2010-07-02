4fb1b809f56f38ded2cc7a13be2ab0787372944b
Richard Klancer <rpk@pobox.com>
2010-07-02 11:46:54 -0400

    LayerIdDidChange fix added to RenderSupport
    Bugfix courtesy of alexiskander

--- frameworks/raphael_views/mixins/raphael_render_support.js
+++ frameworks/raphael_views/mixins/raphael_render_support.js
@@ -15,6 +15,35 @@ RaphaelViews.RenderSupport = {
   render: function (context, firstTime) {
     if (firstTime) this.renderChildViews(context, firstTime);
   },
+  
+  /**
+    Handles changes in the layer id. Modified from SC.View for bugfixes needed by CollectionFastPath mixin.
+    Heads up on the need for this bugfix to alexiskander.
+    
+    Yes, this really works to monkey patch SC.View.layerIdDidChange (observers added by .observers('layerId')
+    are referenced by name)
+    
+  */
+  layerIdDidChange: function() {
+    var layer = this.get("layer");
+    if (layer && this.get("layerId") !== this._lastLayerId) {
+      // if we had an earlier one, remove from view hash. If another view has just claimed our old layerId in the 
+      // views hash, be careful not to overwrite that view. (bugfix per alexiskander)
+      if (this._lastLayerId && SC.View.views[this._lastLayerId] === this) {
+        delete SC.View.views[this._lastLayerId];
+      }
+      
+      // set the current one as the new old one
+      this._lastLayerId = this.get("layerId");
+      
+      // and add the new one
+      SC.View.views[this.get("layerId")] = this;
+      
+      // and finally, set the actual layer id.
+      layer.id = this.get("layerId");
+    }
+  }.observes('layerId'),
+
 
   createLayer: function () {
     if (this.get('layer')) return;          // move along, nothing to do here
