ae8f9c52c93794a01098506cd2cb5c6d27292f91
Richard Klancer <rpk@pobox.com>
2010-07-06 17:05:30 -0400

    Added support for CollectionFastPath to DataPointView
    Turns out that Raphael views need to deal with being put into a "DOM pool" of elements that are kept in the DOM but disconnected from underlying view. Since they won't move offscreen by mechanism CollectionFastPath uses, we hack the view to .hide() and .show() the Raphael object when notified of sleep and wake by CollectionFastPath

--- apps/raphael_demo/views/data_point.js
+++ apps/raphael_demo/views/data_point.js
@@ -22,7 +22,8 @@ RaphaelDemo.DataPointView = RaphaelViews.RaphaelView.extend(
   isEnabled: YES,
   selectedFill: '#0000aa',
   selectedStroke: '#0000aa',
-  layerIsCacheable: YES,            // this is apparently required for the CollectionFastPath mixin to work properly
+  layerIsCacheable: YES,
+  isPoolable: YES,
   
   renderCallback: function (raphaelCanvas, x, y, radius, fill, stroke) {
     return raphaelCanvas.circle(x, y, radius).attr({ fill: fill, stroke: stroke });
@@ -44,6 +45,47 @@ RaphaelDemo.DataPointView = RaphaelViews.RaphaelView.extend(
       circle.attr({ cx: this.getPath('content.x') || 0, cy: this.getPath('content.y') || 0, radius: this.get('radius'), 
         fill: fill, stroke: stroke });
     }
+  },
+  
+  // CollectionFastPath support TODO: move to RaphaelView or RenderSupport
+  
+  /**
+    @private
+    It's necessary to hide the view before it's pooled, because the standard off-screening mechanism used by
+    CollectionFastPath won't work for Raphael views. Note that even though this method is called 'sleepInDOMPool',
+    CollectionFastPath may eventually move this from the DOM pool to an off-DOM pool before it's awakened.
+  */
+  // TODO make this save show/hide state before sleeping
+  sleepInDOMPool: function () {
+    var layer = this.get('layer');
+    if (layer && layer.raphael) layer.raphael.hide();
+  },
+  
+  /**
+    @private
+    Re-show the view if it's been pooled in the DOM.
+  */
+  wakeFromDOMPool: function () {
+    this.wakeFromPool();
+  },
+  
+  /**
+    @private
+    Re-show the view if it's been pooled off-DOM. Note that even though the only sleep method is 'sleepInDOMPool()',
+    we can't therefore conclude that this view will be awakened from the DOM pool. CollectionFastPath moves some views
+    from the DOM pool to the off-DOM pool while they sleep.
+  */
+  awakeFromPool: function () {
+    this.wakeFromPool();
+  },
+  
+  /**
+    @private
+    Re-show the view whether it's been pooled on or off-DOM.
+  */
+  wakeFromPool: function () {
+    var layer = this.get('layer');
+    if (layer && layer.raphael) layer.raphael.show();
   }
 
 });
